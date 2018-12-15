6aa5fbad57150a27198e2276aa792e5d632bba9d
Jindrich Kvita <jindrich.kvita@gnj.cz>
2018-12-15 20:07:17 +0100

    snad vylistovani dokumentu

--- farabuk/obec/views.py
+++ farabuk/obec/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from obec.models import Obec, Album, Foto, Dokument, Uzivatel, Komentar
 
 # Create your views here.
+
+
 def index(request):
     Obce = Obec.objects.all()
     return render(request, 'index.html', {'Obce': Obce})
+
+
+def get_fk(tmp):
+    ck = get_object_or_404(Obec, uri=tmp)
+    return ck.pk
+
+
+def obec_dokument(request, uri):
+    tmp = get_fk(uri)
+    dokumenty = Dokument.objects.filter(uri=tmp)
+    return render(request, 'obec.html', {'dokumenty': dokumenty})
