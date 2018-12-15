93e6cc43984a08094256cd757bd15dc927015d7c
vone <vone@localhost.localdomain>
2018-12-15 13:31:21 +0100

    Removed class Obec in farabuk/models.py

--- farabuk/farabuk/models.py
+++ farabuk/farabuk/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
-
-class obec(models.Model):
-    erb = models.charField(max_lenght = 70)
-    nazev = models.charField(max_lenght = 50)
-    uri = models.charField(max_lenght = 50)
-
-class
-
-
 models
