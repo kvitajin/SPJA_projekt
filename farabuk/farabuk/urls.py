41cf63b3ac4401ddddd99186e29916c505bcbb00
vone <vone@localhost.localdomain>
2018-12-15 20:19:27 +0100

    duno

--- farabuk/farabuk/urls.py
+++ farabuk/farabuk/urls.py
@@ -20,7 +20,6 @@ import obec.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', obec.views.index, name="index"),
-    path('/<obec>/', obec.views.album, name = 'album'),
     path('<uri>', obec.views.obec_dokument, name='obec'),
     path('<obec>/', obec.views.album, name = 'album'),
 
