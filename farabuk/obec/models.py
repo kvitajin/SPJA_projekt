e8ec7f188d6f01bc8cf589dcd3b24fb87b099ee0
Jindrich Kvita <jindrich.kvita@gnj.cz>
2018-12-15 14:35:33 +0100

    pridany formatovane vypisy z models.py

--- farabuk/obec/models.py
+++ farabuk/obec/models.py
@@ -9,10 +9,18 @@ class Obec(models.Model):
     nazev = models.CharField(max_lenght=50)
     uri = models.CharField(max_lenght=50)
 
+    def __str__(self):
+        return "{} {} {}".format(self.nazev, self.erb, self.uri)
+
+
 class Album(models.Model):
     nazev = models.CharFiel(max_lenght=100)
     je_uvodni = models.IntegerField()
     ck_id_obec = models.ForeignKey ('Obec', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}{}{}".format(self.ck_id_obec.nazev, self.nazev, self.je_uvodni)
+
 
 class Foto(models.Model):
     datum = models.DateField(default=date.now)
@@ -20,6 +28,10 @@ class Foto(models.Model):
     nazev_souboru = models.CharField(max_length=70)
     popis = models.CharField()
     ck_id_album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}{}{}{}{}".format(self.ck_id_album.nazev, self.datum, self.sirka, self.nazev_souboru, self.popis)
+
 
 class Dokument(models.Model):
     nadpis = models.CharField()
@@ -29,6 +41,10 @@ class Dokument(models.Model):
     obrazek = models.CharField()
     ck_id_obec = models.ForeignKey('Obec', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{}{}{}{}{}{}".format(self.ck_id_obec.nazev, self.nadpis, self.uri, self.obsah, self.datum_stazeni, self.obrazek)
+
+
 class Uzivatel(models.Model):
     nick = models.CharField()
     heslo = models.CharField()
@@ -37,8 +53,15 @@ class Uzivatel(models.Model):
     ban = models.IntegerField()
     ck_id_obec = models.ForeignKey('Obec', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{}{}{}{}".format(self.email, self.nick, self.nick, self.datum_narozeni, self.ban, self.ck_id_obec.nazev)
+
+
 class Komentar(models.Model):
     obsah = models.CharField(max_lenght=300)
     ck_id_uzivatel = models.ForeignKey('Uzivatel', on_delete=models.CASCADE)
     ck_id_dokument = models.ForeignKey('Dokument', on_delete=models.CASCADE)
     ck_id_foto = models.ForeignKey('Foto', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}{}{}{}".format(self.ck_id_uzivatel.nick, self.obsah, self.ck_id_dokument, self.ck_id_foto)
