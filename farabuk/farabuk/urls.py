daab7e9e980221b98166fbc44632dabcff95c763
Jindrich Kvita <jindrich.kvita@gnj.cz>
2018-12-15 15:59:32 +0100

    index

--- farabuk/farabuk/urls.py
+++ farabuk/farabuk/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import obec.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', obec.views.index, name="index")
+
 ]
