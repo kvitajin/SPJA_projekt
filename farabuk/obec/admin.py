50d70138d65d1f78ee25def652142988dcd6d86c
Jindrich Kvita <jindrich.kvita@gnj.cz>
2018-12-15 15:04:00 +0100

    druha vlna migrantu, kde je nas zachrance Okamura, povolejte domobranu

--- farabuk/obec/admin.py
+++ farabuk/obec/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from obec.models import Obec, Album, Foto, Dokument, Uzivatel, Komentar
 
 # Register your models here.
+
+admin.site.register(Obec)
+admin.site.register(Album)
+admin.site.register(Foto)
+admin.site.register(Dokument)
+admin.site.register(Uzivatel)
+admin.site.register(Komentar)
+
