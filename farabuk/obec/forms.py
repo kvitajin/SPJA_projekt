8e30645d1ad3e8839a8315274d80b3a91904dd33
vone <vone@w118-167.vsb.cz>
2018-12-17 13:52:15 +0100

    Date widget in registruj working

--- farabuk/obec/forms.py
+++ farabuk/obec/forms.py
@@ -6,6 +6,9 @@ class Registruj(forms.ModelForm):
     class Meta:
         model=Uzivatel
         fields=['nick', 'heslo', 'email', 'datum_narozeni', 'ck_id_obec']
+        widgets = {
+        'datum_narozeni': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+    }
         # nick: forms.CharField(max_length=50, label='nick')
         # heslo: forms.CharField(max_length=70, label='heslo')
         # email: forms.CharField(max_length=100, label='email')
